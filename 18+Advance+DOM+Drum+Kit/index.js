8bea9df0ce5a3571acc35077423372a6a0b979c1
Jose Gomez <josgomez6@publicisgroupe.net>
2024-10-20 16:31:02 -0600

    Advance DOM: Drum challenge finished

---
+++ 18+Advance+DOM+Drum+Kit/index.js
@@ -0,0 +1,48 @@
+let drumButtons = document.querySelectorAll(".drum");
+
+drumButtons.forEach(drumButton => {
+    
+    drumButton.addEventListener("click", function (){
+
+        let buttonInnerHtml = this.innerHTML;
+        console.log(buttonInnerHtml)
+
+        switch (buttonInnerHtml) {
+            case "w":
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+
+            case "a":
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+
+            case "s":
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+
+            case "d":
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();    
+
+            case "j":
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "k":
+                let kickBass = new Audio('sounds/kick-bass.mp3');
+                kickBass.play();
+
+            case "l":
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+        
+            default:
+                break;
+        }
+    });
+});
+
+
+
+
